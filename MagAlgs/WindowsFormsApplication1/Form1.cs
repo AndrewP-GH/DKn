f2d66c7e85d015f0a4872fa1c578599e37d8941a
Andrew Paramonov <paravoz.z@yandex.ru>
2017-03-23 00:16:36 +0300

    Begin

---
+++ MagAlgs/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Forms;
+using WindowsFormsApplication1.Properties;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private readonly Type[] _works = null;
+        public Form1()
+        {
+            InitializeComponent();
+            Resources.Culture = CultureInfo.CurrentCulture;
+            StartPosition = FormStartPosition.Manual;
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            //for (var i = 0; i != _works.Length; ++i)
+            //    comboBox1.Items.Add((string)_works[i].GetMethod("ItemText").Invoke(null, null));
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(string.Format(Resources.Form1_оПрограммеToolStripMenuItem_Click_Лабораторная_работа_2017__0_Выполнил__Парамонов_А_А__1_Т3О___101М___16, Environment.NewLine), Resources.Form1_оПрограммеToolStripMenuItem_Click_О_программе, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
